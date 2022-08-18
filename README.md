bb2b237b8829546500eefd078231b3043a9c7d2d
Thibault NOULENS <tnoulens@e1r12p5.clusters.42paris.fr>
2022-08-18 19:12:26 +0200

    ft_env

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 	* [x] pwd sans aucune option
 	* [ ] export sans aucune option
 	* [ ] unset sans aucune option
-	* [ ] env sans aucune option ni argument
+	* [x] env sans aucune option ni argument
 	* [ ] exit sans aucune option
 * Signaux :
 	* [ ] ctrl-C
---
+++ srcs/built-in/ft_env.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_env.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 19:00:00 by tnoulens          #+#    #+#             */
+/*   Updated: 2022/08/18 19:10:46 by tnoulens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	ft_env(char **envp)
+{
+	int	i;
+
+	if (!envp || !*envp)
+		return (1);
+	i = 0;
+	while (envp[i])
+	{
+		ft_putendl_fd(envp[i], 1);
+		++i;
+	}
+	return (0);
+}
+/*
+int	main(int argc, char **argv, char ** envp)
+{
+	(void)argc;
+	(void)argv;
+	ft_env(envp);
+	return (0);
+}*/
