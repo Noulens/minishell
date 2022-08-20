71459497d1fe1a2c141baf1dc29e88bfba0cc1ee
Waxxy <72367316@protonmail.com>
2022-08-20 16:31:12 +0200

    commit gbc update

--- srcs/utilities/garbage_collector.c
+++ srcs/utilities/garbage_collector.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   garbage_collector.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 18:35:15 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/08/19 18:41:32 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/08/20 16:30:23 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	gb_c(t_list **gb, void *content)
 {
-	ft_lstadd_back(gb, ft_lstnew(content));
-	if (!ft_lstlast(*gb)->content)
+	ft_lstadd_front(gb, ft_lstnew(content));
+	if (!(*gb)->content)
 		return (-1);
 	else
 		return (0);
