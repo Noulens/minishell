c33032526f03325656eab03f6cceccec2e0ae2a2
Waxxy <72367316@protonmail.com>
2022-08-22 01:07:20 +0200

    commit gbc update

--- tno_minishell/pipex.c
+++ tno_minishell/pipex.c
@@ -3,16 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 18:31:54 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/08/19 21:07:11 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/08/22 01:07:01 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int	pipex(char **cmd, char **envp)
+{
+	int		i;
+	pid_t	*pid;
+	int		*end;
+
+	i = 0;
+	while (cmd[i])
+		i++;
+	pid = malloc(sizeof(pid_t) * i);
+	end = malloc(sizeof(int) * i - 1);
+	i = 0;
+	while (cmd[i])
+	{
+		if (pipe(end) < 0)
+			return (perror(errno), errno);
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	pid_t	the_pid;
+	return (0);
 }
